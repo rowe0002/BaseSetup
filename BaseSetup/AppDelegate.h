14b248b82fc2e8c36ee707d7ec0144babb3bbb02
rowe0002 <robert.rowe@nyu.edu>
2025-12-30 10:25:37 -0500

    Initial Commit

---
+++ BaseSetup/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BaseSetup
+//
+//  Created by Robert Rowe on 12/30/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BaseSetup/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  BaseSetup
+//
+//  Created by Robert Rowe on 12/30/25.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
